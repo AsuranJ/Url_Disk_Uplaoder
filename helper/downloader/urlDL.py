6e9b2a5bd9f961bc953b119f5ccc34e29f7e6023
AJTimePyro <ajtimepyro@gmail.com>
2021-10-25 12:12:41 +0530

    fixed userid not found issue

--- helper/downloader/urlDL.py
+++ helper/downloader/urlDL.py
@@ -25,8 +25,8 @@ class URLDL:
     async def start(self):
 
         len_file = await length_of_file(self.bot, self.url)
+        self.userid = self.update.chat.id
         if len_file == 'Valid':
-            self.userid = self.update.chat.id
             msg = await self.bot.edit_message_text(self.userid, self.process_msg_id, BotMessage.starting_to_download, parse_mode = 'html')
 
             downObj = SmartDL(self.url, dest = Config.DOWNLOAD_LOCATION)
